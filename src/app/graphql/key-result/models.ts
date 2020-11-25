dd1508544c1c9acceddc4b69cd2e4af4c54c24f0
odelucca <delucca@pm.me>
2020-11-25 11:36:53 -0300

    feature(keyResult): adds format

--- src/app/graphql/key-result/models.ts
+++ src/app/graphql/key-result/models.ts
@@ -1,10 +1,16 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql'
 
 import { ConfidenceReport } from 'app/graphql/confidence-report/models'
 import { Objective } from 'app/graphql/objective/models'
 import { ProgressReport } from 'app/graphql/progress-report/models'
 import { Team } from 'app/graphql/team/models'
 import { User } from 'app/graphql/user/models'
+import { KeyResultFormat } from 'domain/key-result/dto'
+
+registerEnumType(KeyResultFormat, {
+  name: 'KeyResultFormat',
+  description: 'Each format represents how our user wants to see the metrics of the key result',
+})
 
 @ObjectType()
 export class KeyResult {
@@ -22,6 +28,9 @@ export class KeyResult {
 
   @Field(() => Int)
   goal: number
+
+  @Field()
+  format: KeyResultFormat
 
   @Field()
   createdAt: Date
--- src/domain/key-result/dto.ts
+++ src/domain/key-result/dto.ts
@@ -2,12 +2,19 @@ import { ObjectiveDTO } from 'domain/objective/dto'
 import { TeamDTO } from 'domain/team/dto'
 import { UserDTO } from 'domain/user/dto'
 
+export enum KeyResultFormat {
+  NUMBER = 'NUMBER',
+  PERCENTAGE = 'PERCENTAGE',
+  COIN_BRL = 'COIN_BRL',
+}
+
 export class KeyResultDTO {
   id: number
   title: string
   description?: string
   initialValue: number
   goal: number
+  format: KeyResultFormat
   createdAt: Date
   updatedAt: Date
   ownerId: UserDTO['id']
