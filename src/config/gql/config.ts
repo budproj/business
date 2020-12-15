09b0d28366a5ac0524b64f14b32194a14aeb420b
Daniel De Lucca <delucca@pm.me>
2020-12-14 22:16:01 -0300

    Bugfix: GraphQL Playground (#23)
    * test
    
    * test
    
    * fix
    
    * fix: instroscpection
    
    * fix

--- src/config/gql/config.ts
+++ src/config/gql/config.ts
@@ -5,6 +5,7 @@ const { APP_ENV, GRAPHQL_SCHEMA_FILE_PATH } = process.env
 const config: GqlConfigOptions = {
   debug: APP_ENV !== 'production',
   playground: APP_ENV !== 'production',
+  introspection: APP_ENV !== 'production',
   schemaFile: GRAPHQL_SCHEMA_FILE_PATH,
 }
 
--- src/config/gql/types.ts
+++ src/config/gql/types.ts
@@ -2,4 +2,5 @@ export interface GqlConfigOptions {
   debug: boolean
   playground: boolean
   schemaFile: string
+  introspection: boolean
 }
