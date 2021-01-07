f300628e044bd1aeaea3e61c4004e837caa2db9c
delucca <delucca+pc@pm.me>
2021-01-07 20:39:22 -0300

    chore(database): adds sandbox users

---
+++ src/database/environments/production/load-sandbox.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line unicorn/import-style
+import { join } from 'path'
+
+import { runQueryFromFile } from 'database/actions'
+
+const sqlPaths = [join(__dirname, './sandbox-account/users.sql')]
+const queryPromises = sqlPaths.map(runQueryFromFile)
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+queryPromises.reduce(async (previousPromise, nextQuery) => {
+  await previousPromise
+  return nextQuery
+}, Promise.resolve())
