6eeb35cd80065109eac7b115305c87493a4ac014
delucca <delucca+pc@pm.me>
2021-01-19 19:00:20 -0300

    bugfix(repositories): fixes missing constraints in Key Result entity

--- src/domain/repository.ts
+++ src/domain/repository.ts
@@ -1,4 +1,5 @@
-import { Repository, SelectQueryBuilder } from 'typeorm'
+import { flow } from 'lodash'
+import { Brackets, Repository, SelectQueryBuilder, WhereExpression } from 'typeorm'
 
 import { TeamDTO } from 'src/domain/team/dto'
 import { UserDTO } from 'src/domain/user/dto'
@@ -16,19 +17,64 @@ export enum CONDITIONAL_METHOD_NAMES {
 }
 
 abstract class DomainEntityRepository<E> extends Repository<E> {
-  constraintQueryToTeam(
+  composeTeamQuery = flow(this.setupOwnsQuery, this.setupTeamQuery)
+
+  addTeamWhereExpression(
+    _query: WhereExpression,
     _allowedTeams: Array<TeamDTO['id']>,
-    _user: UserDTO,
-    _constraintType?: CONSTRAINT_TYPE,
-  ): SelectionQueryConstrain<E> {
+    _constraintType: CONSTRAINT_TYPE = CONSTRAINT_TYPE.OR,
+  ): WhereExpression {
     throw new Error('You must implement the constraintQueryToTeam method')
   }
 
-  constraintQueryToOwns(
+  addOwnsWhereExpression(
+    _query: WhereExpression,
     _user: UserDTO,
-    _constraintType?: CONSTRAINT_TYPE,
-  ): SelectionQueryConstrain<E> {
-    throw new Error('You must implement the constraintQueryToOwns method')
+    _constraintType: CONSTRAINT_TYPE = CONSTRAINT_TYPE.OR,
+  ): WhereExpression {
+    throw new Error('You must implement the constraintQueryToTeam method')
+  }
+
+  setupTeamQuery(query: SelectQueryBuilder<E>) {
+    return query
+  }
+
+  setupOwnsQuery(query: SelectQueryBuilder<E>) {
+    return query
+  }
+
+  constraintQueryToTeam(allowedTeams: Array<TeamDTO['id']>, user: UserDTO) {
+    const addConstraintToQuery = (query?: SelectQueryBuilder<E>) => {
+      const baseQuery = query ?? this.createQueryBuilder()
+      const composedQuery = this.composeTeamQuery(baseQuery)
+
+      return composedQuery.andWhere(
+        new Brackets((query) => {
+          const teamOwnedEntities = this.addTeamWhereExpression(query, allowedTeams)
+          const userAndTeamOwnedEntities = this.addOwnsWhereExpression(teamOwnedEntities, user)
+
+          return userAndTeamOwnedEntities
+        }),
+      )
+    }
+
+    return addConstraintToQuery
+  }
+
+  constraintQueryToOwns(user: UserDTO) {
+    const addConstraintToQuery = (query?: SelectQueryBuilder<E>) => {
+      const baseQuery = query ?? this.createQueryBuilder()
+
+      return baseQuery.andWhere(
+        new Brackets((query) => {
+          const userOwnedEntities = this.addOwnsWhereExpression(query, user)
+
+          return userOwnedEntities
+        }),
+      )
+    }
+
+    return addConstraintToQuery
   }
 
   selectConditionMethodNameBasedOnConstraintType(constraintType: CONSTRAINT_TYPE) {
