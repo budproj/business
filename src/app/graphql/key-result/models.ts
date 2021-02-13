2915ccb2d759d25fd833a8c24fbb2b535bc2d048
Daniel De Lucca <delucca@pm.me>
2021-02-13 15:50:30 -0300

    bugfix(timeline): fixes progress ignoring 0 (#62)
    Linked issues: BBCR-79@Jira

--- src/app/graphql/key-result/models.ts
+++ src/app/graphql/key-result/models.ts
@@ -27,7 +27,7 @@ export const TimelineEntryUnion = createUnionType({
   name: 'TimelineEntryUnion',
   types: () => [KeyResultCheckInObject, KeyResultCommentObject],
   resolveType(value) {
-    if (value.progress) {
+    if (value.progress || value.progress === 0) {
       return KeyResultCheckInObject
     }
 
