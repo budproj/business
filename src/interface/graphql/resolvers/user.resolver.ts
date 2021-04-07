8f6e646d153a8b8876e6335699fd01d62226ffe2
Daniel De Lucca <delucca@pm.me>
2021-04-07 19:34:07 +0000

    feat(user): finishes partial refactor

--- src/interface/graphql/resolvers/user.resolver.ts
+++ src/interface/graphql/resolvers/user.resolver.ts
@@ -1,5 +1,6 @@
 import { Logger, UseGuards, UseInterceptors } from '@nestjs/common'
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { UserInputError } from 'apollo-server-fastify'
 
 import { Resource } from '@adapters/authorization/enums/resource.enum'
 import { AuthorizationUser } from '@adapters/authorization/interfaces/user.interface'
@@ -10,6 +11,8 @@ import { User } from '@core/modules/user/user.orm-entity'
 import { UsersQueryResultGraphQLObject } from '@interface/graphql/objects/user-query.object'
 import { UserGraphQLObject } from '@interface/graphql/objects/user.object'
 import { UserFiltersRequest } from '@interface/graphql/requests/user/user-filters.request'
+
+import { UserUpdateRequest } from '../requests/user/user-update.request'
 
 import { BaseGraphQLResolver } from './base.resolver'
 import { GraphQLUser } from './decorators/graphql-user'
@@ -47,5 +50,51 @@ export class UserGraphQLResolver extends BaseGraphQLResolver<User, UserInterface
     const queryResult = this.marshalQueryResult(selectedUsers)
 
     return queryResult
+  }
+
+  @RequiredActions('user:read')
+  @Query(() => UserGraphQLObject, { name: 'me' })
+  protected async getMyUser(@GraphQLUser() graphqlUser: AuthorizationUser) {
+    const { id } = graphqlUser
+    this.logger.log(
+      `Fetching data about the user that is executing the request. Provided user ID: ${id.toString()}`,
+    )
+
+    const user = await this.queryGuard.getOneWithActionScopeConstraint({ id }, graphqlUser)
+    if (!user) throw new UserInputError(`We could not found an user with ID ${id}`)
+
+    return user
+  }
+
+  @RequiredActions('user:update')
+  @Mutation(() => UserGraphQLObject, { name: 'updateUser' })
+  protected async updateUser(
+    @GraphQLUser() graphqlUser: AuthorizationUser,
+    @Args() request: UserUpdateRequest,
+  ) {
+    this.logger.log({
+      graphqlUser,
+      request,
+      message: 'Received update user request',
+    })
+
+    const user = await this.queryGuard.updateWithActionScopeConstraint(
+      { id: request.id },
+      request.data,
+      graphqlUser,
+    )
+    if (!user) throw new UserInputError(`We could not found an user with ID ${request.id}`)
+
+    return user
+  }
+
+  @ResolveField('fullName', () => String)
+  protected async getUserFullName(@Parent() user: UserGraphQLObject) {
+    this.logger.log({
+      user,
+      message: 'Fetching user full name',
+    })
+
+    return this.core.user.buildUserFullName(user)
   }
 }
