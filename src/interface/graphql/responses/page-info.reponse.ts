6db1e575f1a1adceafd62e44414b6f1634609a14
Daniel De Lucca <delucca@pm.me>
2021-04-09 21:11:32 +0000

    wip

--- src/interface/graphql/responses/page-info.reponse.ts
+++ src/interface/graphql/responses/page-info.reponse.ts
@@ -1,23 +1,63 @@
-import { NodeGraphQLInterface } from '../interfaces/node.interface'
 import { PageInfoGraphQLObject } from '../objects/page-info.object'
 
-export class PageInfoGraphQLResponse {
-  private readonly count: number
+import { EdgeGraphQLResponse } from './edge.response'
 
-  constructor(nodes: NodeGraphQLInterface[]) {
-    this.count = this.countNodes(nodes)
+interface PageInfoGraphQLResponseProperties<E extends EdgeGraphQLResponse> {
+  edges: E[]
+  count?: number
+  startCursor?: string
+  endCursor?: string
+}
+export class PageInfoGraphQLResponse<E extends EdgeGraphQLResponse = EdgeGraphQLResponse> {
+  constructor(private readonly properties: PageInfoGraphQLResponseProperties<E>) {
+    properties.count = this.getCountFromEdges(properties.edges)
+    properties.startCursor = this.getStartCursorFromEdges(properties.edges)
+    properties.endCursor = this.getEndCursorFromEdges(properties.edges)
+  }
+
+  get edges(): E[] {
+    return this.properties.edges
+  }
+
+  get count(): number | undefined {
+    return this.properties.count
+  }
+
+  get startCursor(): string | undefined {
+    return this.properties.startCursor
+  }
+
+  get endCursor(): string | undefined {
+    return this.properties.endCursor
+  }
+
+  get hasNextPage(): boolean {
+    return false
+  }
+
+  get hasPreviousPage(): boolean {
+    return false
   }
 
   public marshal(): PageInfoGraphQLObject {
     return {
       count: this.count,
-      hasNextPage: false,
-      hasPreviousPage: false,
-      // TECH DEBT: Develop the pagination logic
+      startCursor: this.startCursor,
+      endCursor: this.endCursor,
+      hasNextPage: this.hasNextPage,
+      hasPreviousPage: this.hasPreviousPage,
     }
   }
 
-  private countNodes(nodes: NodeGraphQLInterface[]) {
-    return nodes.length
+  private getCountFromEdges(edges: E[]): number {
+    return edges.length
+  }
+
+  private getStartCursorFromEdges(edges: E[]): string {
+    return edges[0]?.cursor
+  }
+
+  private getEndCursorFromEdges(edges: E[]): string {
+    return edges?.slice(-1)[0]?.cursor
   }
 }
