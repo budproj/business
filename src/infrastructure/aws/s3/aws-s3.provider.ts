64cdd216c954c2364e917cf285c7d8bb3b37af56
root <root@fc36c566-f6b9-44e4-b662-7fc229022aac.qpev5hf2cwnetaadsqb20fhcbc.bx.internal.cloudapp.net>
2021-04-16 17:13:27 +0000

    wip

--- src/infrastructure/aws/s3/aws-s3.provider.ts
+++ src/infrastructure/aws/s3/aws-s3.provider.ts
@@ -3,6 +3,7 @@ import { S3 } from 'aws-sdk'
 import { PutObjectRequest } from 'aws-sdk/clients/s3'
 import { InjectAwsService } from 'nest-aws-sdk'
 
+import { VisibilityStorageEnum } from '@adapters/storage/enums/visilibity.enum'
 import { FilePolicyStorageInterface } from '@adapters/storage/interfaces/file-policy.interface'
 import { RepositoryStorageInterface } from '@adapters/storage/interfaces/repository.interface'
 import { AWSS3ConfigInterface } from '@config/aws/aws.interface'
@@ -14,6 +15,7 @@ import { RemoteFileAWSS3Interface } from './interfaces/remote-file.interface'
 @Injectable()
 export class AWSS3Provider implements RepositoryStorageInterface {
   private readonly config: AWSS3ConfigInterface
+  private readonly urlPrefix: string
 
   constructor(
     @InjectAwsService(S3)
@@ -21,13 +23,14 @@ export class AWSS3Provider implements RepositoryStorageInterface {
     awsConfig: AWSConfigProvider,
   ) {
     this.config = awsConfig.s3
+    this.urlPrefix = this.getURLPrefix(awsConfig.region)
   }
 
   public async upload(file: FileAWSS3Interface): Promise<RemoteFileAWSS3Interface> {
     const key = this.generateFileKey(file)
     const acl = this.marshalACL(file.policy)
 
-    const uploaded = await this.remote
+    await this.remote
       .putObject({
         Bucket: this.config.bucketName,
         Key: key,
@@ -37,14 +40,14 @@ export class AWSS3Provider implements RepositoryStorageInterface {
       })
       .promise()
 
-    console.log('ok')
-    console.log(uploaded)
-    // TODO: Falta:
-    // - Arrumar o ACL pra ficar público
-    // - Arrumar tb o key pra usar o diretório correto
-    // - Pegar a URL e retornar ela pra salvar no banco
+    const response = this.buildRemoteFileResponse(key)
+    console.log(response)
 
-    return {} as any
+    return response
+  }
+
+  private getURLPrefix(region: string): string {
+    return `https://s3-${region}.amazonaws.com`
   }
 
   private generateFileKey(file: FileAWSS3Interface): string {
@@ -54,7 +57,20 @@ export class AWSS3Provider implements RepositoryStorageInterface {
     return key
   }
 
-  private marshalACL(policy: FilePolicyStorageInterface): PutObjectRequest['ACL'] {
-    return 'public-read'
+  private marshalACL(policy?: FilePolicyStorageInterface): PutObjectRequest['ACL'] {
+    if (policy?.write === VisibilityStorageEnum.PUBLIC) return 'public-read-write'
+    if (policy?.read === VisibilityStorageEnum.PUBLIC) return 'public-read'
+
+    return 'private'
+  }
+
+  private buildRemoteFileResponse(key: string): RemoteFileAWSS3Interface {
+    return {
+      path: this.getObjectURL(key),
+    }
+  }
+
+  private getObjectURL(fileKey: string): string {
+    return `${this.urlPrefix}/${this.config.bucketName}/${fileKey}`
   }
 }
