0d9c3118fc1f13907d81a046960631bb419edd30
Daniel De Lucca <delucca@pm.me>
2021-05-06 20:04:08 +0000

    wip

--- src/core/ports/commands/command.factory.ts
+++ src/core/ports/commands/command.factory.ts
@@ -5,8 +5,10 @@ import { CoreProvider } from '@core/core.provider'
 import { Command } from './base.command'
 import { CreateCheckInCommand } from './create-check-in.command'
 import { GetCheckInWindowForCheckInCommand } from './get-check-in-window-for-check-in.command'
+import { GetCycleCommand } from './get-cycle.command'
 import { GetKeyResultCheckInListCommand } from './get-key-result-check-in-list.command'
 import { GetKeyResultCompanyCommand } from './get-key-result-company.command'
+import { GetKeyResultCycleCommand } from './get-key-result-cycle.command'
 import { GetKeyResultFromCheckInCommand } from './get-key-result-from-check-in.command'
 import { GetKeyResultOwnerCommand } from './get-key-result-owner.command'
 import { GetKeyResultTeamTreeCommand } from './get-key-result-team-tree.command'
@@ -26,6 +28,8 @@ export type CommandType =
   | 'get-team-owner'
   | 'get-user-companies'
   | 'get-check-in-window-for-check-in'
+  | 'get-cycle'
+  | 'get-key-result-cycle'
 
 @Injectable()
 export class CommandFactory {
@@ -40,6 +44,8 @@ export class CommandFactory {
     'get-team-owner': GetTeamOwnerCommand,
     'get-user-companies': GetUserCompaniesCommand,
     'get-check-in-window-for-check-in': GetCheckInWindowForCheckInCommand,
+    'get-cycle': GetCycleCommand,
+    'get-key-result-cycle': GetKeyResultCycleCommand,
   }
 
   constructor(private readonly core: CoreProvider) {}
--- src/infrastructure/amplitude/events/created-check-in.event.ts
+++ src/infrastructure/amplitude/events/created-check-in.event.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 import { Activity } from '@adapters/activity/activities/base.activity'
 import { CREATED_CHECK_IN_ACTIVITY_TYPE } from '@adapters/activity/activities/created-check-in-activity'
+import { Cycle } from '@core/modules/cycle/cycle.orm-entity'
 import { KeyResultCheckIn } from '@core/modules/key-result/check-in/key-result-check-in.orm-entity'
 import { KeyResult } from '@core/modules/key-result/key-result.orm-entity'
 import { CorePortsProvider } from '@core/ports/ports.provider'
@@ -13,21 +14,21 @@ type CreatedCheckInAmplitudeEventProperties = {
   keyResultFormat?: 'NUMBER' | 'PERCENTAGE' | 'COIN_BRL' | 'COIN_USD'
   isFirst?: boolean
   minutesSinceLastCheckIn?: number
-  keyResultCycleCadence?: 'QUARTERLY' | 'YEARLY'
-  progressChanged?: boolean
-  previousProgress?: number
-  newProgress?: number
+  cycleCadence?: 'QUARTERLY' | 'YEARLY'
   deltaProgress?: number
+  progressChanged?: boolean
+  deltaConfidence?: number
   confidenceChanged?: boolean
-  oldConfidence?: 'HIGH' | 'MEDIUM' | 'LOW' | 'BARRIER'
-  newConfidence?: 'HIGH' | 'MEDIUM' | 'LOW' | 'BARRIER'
   hasComment?: boolean
   commentLength?: number
+  company?: string
+  team?: string
 }
 
 type RelatedData = {
   keyResult: KeyResult
   keyResultCheckInList: KeyResultCheckIn[]
+  cycle: Cycle
 }
 
 @Injectable()
@@ -43,23 +44,26 @@ export class CreatedCheckInAmplitudeEvent extends BaseAmplitudeEvent<
   }
 
   public async loadProperties(): Promise<void> {
-    const { keyResult, keyResultCheckInList } = await this.getRelatedData()
+    const { keyResult, keyResultCheckInList, cycle } = await this.getRelatedData()
 
     this.properties = {
       isOwner: this.isUserOwner(keyResult),
       keyResultFormat: keyResult.format,
       isFirst: this.isFirst(keyResultCheckInList),
       minutesSinceLastCheckIn: await this.getMinutesSinceLastCheckIn(),
+      cycleCadence: cycle.cadence,
     }
   }
 
   private async getRelatedData(): Promise<RelatedData> {
     const keyResult = await this.getKeyResult()
     const keyResultCheckInList = await this.getKeyResultCheckInList(keyResult)
+    const cycle = await this.getCycle(keyResult)
 
     return {
       keyResult,
       keyResultCheckInList,
+      cycle,
     }
   }
 
@@ -71,6 +75,10 @@ export class CreatedCheckInAmplitudeEvent extends BaseAmplitudeEvent<
 
   private async getKeyResultCheckInList(keyResult: KeyResult): Promise<KeyResultCheckIn[]> {
     return this.core.dispatchCommand<KeyResultCheckIn[]>('get-key-result-check-in-list', keyResult)
+  }
+
+  private async getCycle(keyResult: KeyResult): Promise<Cycle> {
+    return this.core.dispatchCommand<Cycle>('get-key-result-cycle', keyResult)
   }
 
   private isUserOwner(keyResult: KeyResult): boolean {
