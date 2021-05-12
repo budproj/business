1c26e9b4a3265605b5f65e4c2bd993dc3673062d
Daniel De Lucca <delucca@pm.me>
2021-05-12 14:20:07 +0000

    bugfix: fixing comment policy (#98)

--- src/interface/graphql/modules/key-result/access-control/key-result-comment.access-control.ts
+++ src/interface/graphql/modules/key-result/access-control/key-result-comment.access-control.ts
@@ -3,8 +3,17 @@ import { Injectable } from '@nestjs/common'
 import { AccessControlScopes } from '@adapters/authorization/interfaces/access-control-scopes.interface'
 import { Resource } from '@adapters/policy/enums/resource.enum'
 import { UserWithContext } from '@adapters/state/interfaces/user.interface'
+import { KeyResultComment } from '@core/modules/key-result/comment/key-result-comment.orm-entity'
+import { KeyResult } from '@core/modules/key-result/key-result.orm-entity'
+import { Team } from '@core/modules/team/team.orm-entity'
 import { CorePortsProvider } from '@core/ports/ports.provider'
 import { KeyResultBaseAccessControl } from '@interface/graphql/modules/key-result/access-control/base.access-control'
+
+type RelatedEntities = {
+  keyResultComment: KeyResultComment
+  keyResult: KeyResult
+  teams: Team[]
+}
 
 @Injectable()
 export class KeyResultCommentAccessControl extends KeyResultBaseAccessControl {
@@ -12,6 +21,10 @@ export class KeyResultCommentAccessControl extends KeyResultBaseAccessControl {
 
   constructor(protected core: CorePortsProvider) {
     super(core)
+  }
+
+  static isCommentAuthor(keyResultComment: KeyResultComment, user: UserWithContext): boolean {
+    return keyResultComment.userId === user.id
   }
 
   protected async resolveContextScopes(
@@ -22,13 +35,36 @@ export class KeyResultCommentAccessControl extends KeyResultBaseAccessControl {
   }
 
   protected async resolveEntityScopes(
-    _user: UserWithContext,
-    _keyResultCommentID: string,
+    user: UserWithContext,
+    keyResultCommentID: string,
   ): Promise<AccessControlScopes> {
+    const { keyResultComment, keyResult, teams } = await this.getRelatedEntities(keyResultCommentID)
+
+    const isCommentAuthor = KeyResultCommentAccessControl.isCommentAuthor(keyResultComment, user)
+    const isTeamLeader = await this.isTeamLeader(teams, user)
+    const isCompanyMember = await this.isKeyResultCompanyMember(keyResult, user)
+
     return {
-      isOwner: false,
-      isTeamLeader: false,
-      isCompanyMember: false,
+      isTeamLeader,
+      isCompanyMember,
+      isOwner: isCommentAuthor,
+    }
+  }
+
+  private async getRelatedEntities(keyResultCommentID: string): Promise<RelatedEntities> {
+    const keyResultComment = await this.core.dispatchCommand<KeyResultComment>(
+      'get-key-result-comment',
+      { id: keyResultCommentID },
+    )
+    const keyResult = await this.core.dispatchCommand<KeyResult>('get-key-result', {
+      id: keyResultComment.keyResultId,
+    })
+    const teams = await this.getKeyResultTeamTree(keyResult)
+
+    return {
+      keyResultComment,
+      keyResult,
+      teams,
     }
   }
 }
