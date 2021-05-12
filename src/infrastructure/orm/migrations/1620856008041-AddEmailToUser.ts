5622dbe2ed2e04430b4ded29aeee33de19d90c30
delucca <delucca@pm.me>
2021-05-12 19:01:30 -0300

    chore(orm): fixes cli config

--- src/infrastructure/orm/migrations/1620856008041-AddEmailToUser.ts
+++ src/infrastructure/orm/migrations/1620856008041-AddEmailToUser.ts
@@ -4,9 +4,10 @@ export class AddEmailToUser1620856008041 implements MigrationInterface {
   name = 'AddEmailToUser1620856008041'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS citext`)
     await queryRunner.query(`ALTER TABLE "user" ADD "email" citext`)
-    await queryRunner.query(`UPDATE "user" SET email="unknown@unknown"`)
-    await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "email" SET NOT_NULL`)
+    await queryRunner.query(`UPDATE "user" SET email='unknown@unknown'`)
+    await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "email" SET NOT NULL`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
