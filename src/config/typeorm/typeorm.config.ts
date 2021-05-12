ca70f3afb031a664df7aa7c8004b6b3c124b3a5c
delucca <delucca@pm.me>
2021-05-12 17:17:22 -0300

    chore(config): enables TypeORM CLI config

--- src/config/typeorm/typeorm.config.ts
+++ src/config/typeorm/typeorm.config.ts
@@ -3,58 +3,42 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
 import { TypeORMConfigInterface } from './typeorm.interface'
 
-const config = {
-  type: process.env.TYPEORM_CONNECTION,
-  database: process.env.TYPEORM_DATABASE,
-  host: process.env.TYPEORM_HOST,
-  port: Number.parseInt(process.env.TYPEORM_PORT, 10),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  migrations: process.env.TYPEORM_MIGRATIONS.split(','),
-  entities: process.env.TYPEORM_ENTITIES.split(','),
-  logging: process.env.TYPEORM_LOGGING_ENABLED === 'true',
-  namingStrategy:
-    process.env.TYPEORM_CONVENTION_NAMING_ENABLED === 'true'
-      ? new SnakeNamingStrategy()
-      : undefined,
-  cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
-  },
-}
-
 export const typeormConfig = registerAs(
   'typeorm',
   (): TypeORMConfigInterface => ({
-    type: config.type,
+    type: process.env.TYPEORM_CONNECTION,
 
     endpoint: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
+      host: process.env.TYPEORM_HOST,
+      port: Number.parseInt(process.env.TYPEORM_PORT, 10),
+      database: process.env.TYPEORM_DATABASE,
     },
 
     authentication: {
-      user: config.username,
-      password: config.password,
+      user: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
     },
 
     pattern: {
       file: {
-        entities: config.entities,
-        migrations: config.migrations,
+        entities: process.env.TYPEORM_ENTITIES.split(','),
+        migrations: process.env.TYPEORM_MIGRATIONS.split(','),
       },
 
       directory: {
-        migrations: config.cli.migrationsDir,
+        migrations: process.env.TYPEORM_MIGRATIONS_DIR,
       },
     },
 
     logging: {
-      enabled: config.logging,
+      enabled: process.env.TYPEORM_LOGGING_ENABLED === 'true',
     },
 
     conventions: {
-      naming: config.namingStrategy,
+      naming:
+        process.env.TYPEORM_CONVENTION_NAMING_ENABLED === 'true'
+          ? new SnakeNamingStrategy()
+          : undefined,
     },
   }),
 )
