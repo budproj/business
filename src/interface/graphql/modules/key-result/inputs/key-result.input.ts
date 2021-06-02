a92afcadf0b44893d83d82ee31cd81db0628c8d2
delucca <delucca@pm.me>
2021-06-02 13:28:55 -0300

    fixing null

--- src/interface/graphql/modules/key-result/inputs/key-result.input.ts
+++ src/interface/graphql/modules/key-result/inputs/key-result.input.ts
@@ -7,35 +7,33 @@ import { KeyResultTypeGraphQLEnum } from '@interface/graphql/modules/key-result/
 
 @InputType('KeyResultInput', { description: 'Data required to create a new Key Result' })
 export class KeyResultInput {
-  @Field({ description: 'The title of the key result', nullable: true })
+  @Field({ description: 'The title of the key result' })
   public readonly title: string
 
-  @Field(() => Float, { description: 'The initial value of the key result', nullable: true })
+  @Field(() => Float, { description: 'The initial value of the key result' })
   public readonly initialValue: number
 
-  @Field(() => Float, { description: 'The goal of the key result', nullable: true })
+  @Field(() => Float, { description: 'The goal of the key result' })
   public readonly goal: number
 
   @Field(() => KeyResultFormatGraphQLEnum, {
-    complexity: 0,
     description: 'The format of the key result',
   })
   public readonly format: KeyResultFormat
 
   @Field(() => KeyResultTypeGraphQLEnum, {
-    complexity: 0,
     description: 'The type of the key result',
     defaultValue: KeyResultType.ASCENDING,
   })
   public readonly type: KeyResultType
 
-  @Field(() => ID, { description: 'The owner ID of the key result', nullable: true })
+  @Field(() => ID, { description: 'The owner ID of the key result' })
   public readonly ownerId: string
 
-  @Field(() => ID, { description: 'The object ID that this key result belongs to', nullable: true })
+  @Field(() => ID, { description: 'The object ID that this key result belongs to' })
   public readonly objectiveId: string
 
-  @Field(() => ID, { description: 'The team ID that this key result belongs to', nullable: true })
+  @Field(() => ID, { description: 'The team ID that this key result belongs to' })
   public readonly teamId: string
 
   @Field({
