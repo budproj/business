e52b2d9be0e59a936b9da29a83dd32cc6750a778
Victor Perin <me@victorperin.ninja>
2021-07-28 15:22:20 -0300

    init(package): add jest test setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig.json')
+
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+
+  // jest cant load custom module paths from typescript, so moduleNameMapper is required
+  // more here: https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping/
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
