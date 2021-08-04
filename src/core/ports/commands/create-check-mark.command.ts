1458749856cd934f34a6877e012e2f56d9669964
Victor Perin <me@victorperin.ninja>
2021-08-04 20:44:43 -0300

    refactor(core/ports/commands/create-check-mark): simplify input

--- src/core/ports/commands/create-check-mark.command.ts
+++ src/core/ports/commands/create-check-mark.command.ts
@@ -1,9 +1,25 @@
+import { CheckMarkStates } from '@core/modules/key-result/check-mark/key-result-check-mark.interface'
 import { KeyResultCheckMark } from '@core/modules/key-result/check-mark/key-result-check-mark.orm-entity'
+import { User } from '@core/modules/user/user.orm-entity'
 
 import { Command } from './base.command'
 
+interface CommandInput {
+  checkMark: Partial<KeyResultCheckMark>
+  user: Partial<User>
+}
+
 export class CreateCheckMarkCommand extends Command<KeyResultCheckMark> {
-  public async execute(checkMark: KeyResultCheckMark): Promise<KeyResultCheckMark> {
-    return this.core.keyResult.keyResultCheckMarkProvider.createCheckMark(checkMark)[0]
+  public async execute({ checkMark, user }: CommandInput): Promise<KeyResultCheckMark> {
+    const builtCheckMark = {
+      description: checkMark.description,
+      state: CheckMarkStates.UNCHECKED,
+      keyResultId: checkMark.keyResultId,
+      userId: user.id,
+    }
+
+    const newCheckMark = await this.core.keyResult.keyResultCheckMarkProvider.createCheckMark(builtCheckMark)
+
+    return newCheckMark[0]
   }
 }
