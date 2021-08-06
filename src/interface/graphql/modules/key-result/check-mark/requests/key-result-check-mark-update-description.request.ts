89e69ad5fb8d3ceb5c166a16f39916321ea37bce
delucca <delucca@protonmail.com>
2021-08-06 11:48:25 -0300

    bugfix: fixing ID

--- src/interface/graphql/modules/key-result/check-mark/requests/key-result-check-mark-update-description.request.ts
+++ src/interface/graphql/modules/key-result/check-mark/requests/key-result-check-mark-update-description.request.ts
@@ -1,4 +1,4 @@
-import { ArgsType, Field } from '@nestjs/graphql';
+import { ArgsType, Field, ID } from '@nestjs/graphql';
 import { KeyResultCheckMarkUpdateDescriptionInputObject } from '../objects/key-result-check-mark-input.objects';
 
 
