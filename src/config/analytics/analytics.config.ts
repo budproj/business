0e0ce10fb6dadd6fb16d822885aa102c76b28ab1
delucca <delucca@protonmail.com>
2021-08-30 19:18:13 -0300

    fix

--- src/config/analytics/analytics.config.ts
+++ src/config/analytics/analytics.config.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line unicorn/import-style
 import { join } from 'path'
 
+import { ServerCredentials } from '@grpc/grpc-js'
 import { registerAs } from '@nestjs/config'
 
 import { AnalyticsConfig } from './analytics.interface'
@@ -10,6 +11,7 @@ export const analyticsConfig = registerAs(
   (): AnalyticsConfig => ({
     grpcServer: {
       url: process.env.ANALYTICS_URL,
+      credentials: ServerCredentials.createInsecure(),
       package: process.env.ANALYTICS_PACKAGES.split(','),
       protoPath: process.env.ANALYTICS_PACKAGES.split(',').map((packageName) =>
         join(__dirname, `../../../lib/grpc/analytics/${packageName}.proto`),
--- src/config/analytics/analytics.interface.ts
+++ src/config/analytics/analytics.interface.ts
@@ -1,9 +1,12 @@
+import { ServerCredentials } from '@grpc/grpc-js'
+
 export interface AnalyticsConfig {
   grpcServer: AnalyticsGRPCServerConfig
 }
 
 export interface AnalyticsGRPCServerConfig {
   url: string
+  credentials: ServerCredentials
   package: string[]
   protoPath: string[]
 }
