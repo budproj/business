e8f03d63368ad7583b88d81e70d749bb3a30e45a
diegomagalhaes-dev <digengup@gmail.com>
2022-03-31 18:10:20 -0300

    feat(infra) sentry

--- src/infrastructure/sentry/sentry.module.ts
+++ src/infrastructure/sentry/sentry.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common'
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { RavenInterceptor, RavenModule } from 'nest-raven'
 
+import { SentryConfigModule } from '@config/sentry/sentry.module'
+
+import { SentryProvider } from './sentry.provider'
+
+const RavenProvider = { provide: APP_INTERCEPTOR, useValue: new RavenInterceptor() }
+
 @Module({
-  imports: [RavenModule],
-  providers: [{ provide: APP_INTERCEPTOR, useValue: new RavenInterceptor() }],
+  imports: [SentryConfigModule, RavenModule],
+  providers: [SentryProvider, RavenProvider],
 })
 export class SentryModule {}
