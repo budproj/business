0bf11d253d70a9ac93928904c45e872333d7df5e
diego magalhaes <digengup@gmail.com>
2022-05-17 16:52:06 -0300

    fix(refactor/objective-resolver) update fields description

--- src/interface/graphql/modules/objective/requests/objective-filters.request.ts
+++ src/interface/graphql/modules/objective/requests/objective-filters.request.ts
@@ -13,14 +13,14 @@ export class ObjectiveFiltersRequest extends ConnectionFiltersRequest {
 
   @Field(() => ID, {
     description:
-      'If you set this argument to true or false it will only fetch active or non-active objectives. Leave it null if you want both',
+      'If you set this argument to a user id, it will fetch its objectives. Leave null if you want all objectives.',
     nullable: true,
   })
   public readonly ownerId: string
 
   @Field(() => ID, {
     description:
-      'If you set this argument to true or false it will only fetch active or non-active objectives. Leave it null if you want both',
+      'If you set this argument with a team id, it will fetch team related objectives. Leave null if you want all goals or set the id to null if you want objectives without team id.',
     nullable: true,
   })
   public readonly teamId: string
