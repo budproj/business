9e1c7d4690e754d1ab2d9105a78c46eae32d68e1
Victor Perin <me@victorperin.ninja>
2022-05-31 18:52:52 -0300

    bugfix(kr/access-control): read individual okrs from other users in the same company

--- src/interface/graphql/modules/key-result/access-control/base.access-control.ts
+++ src/interface/graphql/modules/key-result/access-control/base.access-control.ts
@@ -22,9 +22,7 @@ export abstract class KeyResultBaseAccessControl extends AccessControl {
 
     const isKeyResultOwner = await this.isKeyResultOwner(keyResult, user)
     const isTeamLeader = await this.isTeamLeader(teams, user)
-    const isCompanyMember = keyResult.teamId
-      ? await this.isKeyResultCompanyMember(keyResult, user)
-      : false
+    const isCompanyMember = await this.isKeyResultCompanyMember(keyResult, user)
 
     return {
       isTeamLeader,
