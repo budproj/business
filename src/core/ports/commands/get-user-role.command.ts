710bb4f7b013246f59b8a7fdfbc68e76684ddc5e
Rodrigo Antas <rodr.antas@gmail.com>
2022-06-08 15:09:50 -0300

    feat(user-role): changing command to get id instead of authzsubid

--- src/core/ports/commands/get-user-role.command.ts
+++ src/core/ports/commands/get-user-role.command.ts
@@ -3,7 +3,8 @@ import { Role } from 'auth0'
 import { Command } from './base.command'
 
 export class GetUserRoleCommand extends Command<Role> {
-  public async execute(authzSubUserId: string): Promise<Role> {
-    return this.core.user.credentials.getUserRole(authzSubUserId)
+  public async execute(id: string): Promise<Role> {
+    const user = await this.core.user.getOne({ id })
+    return this.core.user.credentials.getUserRole(user.authzSub)
   }
 }
