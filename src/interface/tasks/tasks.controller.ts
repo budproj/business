a646fce00d64b5ab91c12223f002333420ffb622
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-09-03 22:38:58 -0300

    feat(tasks-module): implements the controller that exports the ports

--- src/interface/tasks/tasks.controller.ts
+++ src/interface/tasks/tasks.controller.ts
@@ -1,15 +1,41 @@
 import { Controller, Logger } from '@nestjs/common'
-import { MessagePattern, Payload, Transport } from '@nestjs/microservices'
+import { Ctx, MessagePattern, NatsContext, Payload } from '@nestjs/microservices'
+
+import { isOfTypeCommand } from '@core/ports/commands/command.factory'
+import { CorePortsProvider } from '@core/ports/ports.provider'
 
 @Controller()
 export class TasksController {
   private readonly logger = new Logger(TasksController.name)
 
-  @MessagePattern('dummy', Transport.NATS)
-  dummy(@Payload() data: string): void {
+  constructor(private readonly corePorts: CorePortsProvider) {}
+
+  @MessagePattern('core-ports.*')
+  async useUsersPorts(@Payload() data: string, @Ctx() context: NatsContext): Promise<unknown> {
+    const messageName = context.getSubject()
+
+    this.logger.log({
+      message: `New ${messageName} message received`,
+    })
+
+    const [_, commandName] = messageName.split('.')
+
+    if (!commandName || !isOfTypeCommand(commandName)) {
+      return 'Invalid Command'
+    }
+
     this.logger.log({
       data,
-      message: 'New dummy message received',
+      message: `Executing the ${commandName} command`,
     })
+
+    const dataReturned = await this.corePorts.dispatchCommand<unknown>(commandName, data)
+
+    this.logger.log({
+      data: dataReturned,
+      message: `${commandName} data retunerd`,
+    })
+
+    return dataReturned
   }
 }
--- src/interface/tasks/tasks.module.ts
+++ src/interface/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 
+import { CoreModule } from '@core/core.module'
 import { TasksController } from '@interface/tasks/tasks.controller'
 
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [TasksController],
   providers: [],
 })
