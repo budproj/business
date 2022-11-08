fb4ab69a3e35e853c1297a100de316c25763ac4d
Victor Perin <me@victorperin.ninja>
2022-11-08 13:23:55 -0300

    chore(package): update aws dependencies

--- src/infrastructure/aws/aws.factory.ts
+++ src/infrastructure/aws/aws.factory.ts
@@ -3,6 +3,7 @@ import { AwsServiceConfigurationOptionsFactory } from 'nest-aws-sdk'
 import { AWSConfigProvider } from '@config/aws/aws.provider'
 
 export function awsFactory(config: AWSConfigProvider): AwsServiceConfigurationOptionsFactory {
+  console.log('AWS CREDENTIALS', config.credentials.accessKeyID, config.credentials.secretAccessKey)
   return {
     region: config.region,
     credentials: {
