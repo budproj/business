f990e48ba245ab106016ca7c15af55849d8d138e
Danilo Valente <danilovalente96@gmail.com>
2023-06-23 15:31:10 -0300

    fix(routines): fix routines notifications (#266)

--- src/core/ports/commands/check-if-user-has-pending-key-results.command.ts
+++ src/core/ports/commands/check-if-user-has-pending-key-results.command.ts
@@ -1,26 +1,36 @@
-import { Status } from '@core/interfaces/status.interface'
 import { KeyResult } from '@core/modules/key-result/key-result.orm-entity'
 import { User } from '@core/modules/user/user.orm-entity'
+import { BaseStatusCommand } from '@core/ports/commands/base-status.command';
 
 import { Command } from './base.command'
 
-export class CheckIfUserHasPendingKeyResultCommand extends Command<number> {
-  public async execute(id: User['id']): Promise<number> {
+/**
+ * @deprecated fastest way to reuse logic without dealing with the high coupling between business logic and abstract command classes
+ */
+class CheckIfKeyResultIsOutdated extends BaseStatusCommand {
+  // Ignoring, as we have no other way to deal with this (WARNING: extremely hacky way)
+  // @ts-ignore
+  async execute(keyResult: KeyResult): Promise<boolean> {
+    const latestCheckIn = await this.core.keyResult.getLatestCheckInForKeyResultAtDate(keyResult.id)
+
+    return this.isOutdated(latestCheckIn, new Date(), keyResult.createdAt)
+  }
+}
+
+export class CheckIfUserHasPendingKeyResultCommand extends Command<boolean> {
+  public async execute(id: User['id']): Promise<boolean> {
     const getUserKeyResults = this.factory.buildCommand<KeyResult[]>('get-user-key-results')
-    const getKeyResultStatus = this.factory.buildCommand<Status>('get-key-result-status')
+    const checkIfKeyResultIsOutdated = new CheckIfKeyResultIsOutdated(this.core, this.factory)
 
     const userKeyResults = await getUserKeyResults.execute(id, {})
 
-    const pendingKeyResults = await userKeyResults.reduce(async (promise, keyResult) => {
-      const previous = await promise
+    for (const keyResult of userKeyResults) {
+      const isOutdated = await checkIfKeyResultIsOutdated.execute(keyResult)
+      if (isOutdated) {
+        return true
+      }
+    }
 
-      const { isOutdated } = await getKeyResultStatus.execute(keyResult.id)
-
-      if (isOutdated) return [...previous, keyResult]
-
-      return previous
-    }, Promise.resolve([]))
-
-    return pendingKeyResults.length
+    return false
   }
 }
