bbdf92dc8bed49aed4bbe8f5fbb3eb3ed765c8f0
Danilo Valente <danilovalente96@gmail.com>
2023-06-23 12:47:07 -0300

    config(logs): add logs to notification ports

--- src/infrastructure/notification/notification.provider.ts
+++ src/infrastructure/notification/notification.provider.ts
@@ -3,6 +3,7 @@ import { Injectable, Logger } from '@nestjs/common'
 import { Activity } from '@adapters/activity/activities/base.activity'
 import { ActivityDispatcher } from '@adapters/activity/interfaces/activity-dispatcher.interface'
 import { NotificationFactory } from '@infrastructure/notification/notifications/notification.factory'
+import { Stopwatch } from '@lib/logger/pino.decorator';
 
 @Injectable()
 export class NotificationProvider implements ActivityDispatcher {
@@ -10,6 +11,7 @@ export class NotificationProvider implements ActivityDispatcher {
 
   constructor(private readonly notifications: NotificationFactory) {}
 
+  @Stopwatch()
   public async dispatch<D = any>(activity: Activity<D>): Promise<void> {
     const notification = this.notifications.buildNotificationForActivity(activity)
     if (!notification) return
