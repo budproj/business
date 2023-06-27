3c3ec36a8d866ab87ac17c5688b4ee59bfa56044
diego magalhaes <digengup@gmail.com>
2023-06-27 12:57:59 -0300

    feat(llm-domain) implementing authentication control to LLMs module

--- src/interface/interface.module.ts
+++ src/interface/interface.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 
 import { PingController } from '@interface/ping.controller'
 import { TasksModule } from '@interface/tasks/tasks.module'
+import { LLMsDomainModule } from 'src/llm/llms-domain.module'
 
 import { GraphQLModule } from './graphql/graphql.module'
 
 @Module({
-  imports: [GraphQLModule, TasksModule],
+  imports: [GraphQLModule, TasksModule, LLMsDomainModule],
   controllers: [PingController],
 })
 export class InterfaceModule {}
