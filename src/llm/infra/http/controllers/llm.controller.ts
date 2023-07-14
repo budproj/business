449e54734a147dff83c038a2c35f48a45bcfc489
Danilo Valente <danilovalente96@gmail.com>
2023-07-14 02:21:59 -0300

    feat(llm): upgrade key result summary prompt to v1.1

--- src/llm/infra/http/controllers/llm.controller.ts
+++ src/llm/infra/http/controllers/llm.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { ActionType, TargetEntity } from '@prisma/client'
-import { CreateChatCompletionRequest } from 'openai'
 
 import { Stopwatch } from '@lib/logger/pino.decorator'
-import OpenAICompletionService from 'src/llm/domain/open-ai/services/open-ai-completion.service'
-import { buildMessages } from 'src/llm/shared/utilities/summarize-key-result-prompt-builder'
+import OpenAICompletionService, {
+  CompletionRequestPrompt,
+} from 'src/llm/domain/open-ai/services/open-ai-completion.service'
+import * as summarizeKeyResult from 'src/llm/shared/utilities/summarize-key-result-prompt-builder'
 import { SummarizeKeyResultInput } from 'src/llm/shared/utilities/summarize-key-result.types'
 import { Author } from 'src/llm/shared/utilities/types'
 
@@ -35,12 +36,25 @@ export class LLMsController {
       teamId: body.author.teamId,
     }
 
-    const messages = buildMessages(input, true, locale ?? 'pt-BR')
+    const messages = summarizeKeyResult.buildMessages(input, true, locale ?? 'pt-BR')
 
-    const prompt: CreateChatCompletionRequest = {
+    const draftPrompt: CompletionRequestPrompt = {
       model: 'gpt-3.5-turbo',
       temperature: 0.5,
       messages,
+    }
+
+    // Check this document for an explanation of the formula:
+    // https://www.notion.so/budops/Spotlight-P-s-release-d57d9e57f42a49e18633018186e9d023?pvs=4#f5b30c2249bb4135af3fd257b648e28f
+    const promptTokens = this.openAiCompletionService.estimatePromptTokens(draftPrompt).count
+    const completionTokens = 74.8 * promptTokens ** 0.006
+    const exceedsContextWindowSize = promptTokens + completionTokens > 4096
+
+    // Try to estimate the total number of tokens that will be used by the completion
+    // If it exceeds the 4k tokens context window, switch to gpt-3.5-turbo-16k
+    const prompt: CompletionRequestPrompt = {
+      ...draftPrompt,
+      model: exceedsContextWindowSize ? 'gpt-3.5-turbo-16k' : draftPrompt.model,
     }
 
     const { output, model, respondedAt } = await this.openAiCompletionService.complete(
@@ -50,7 +64,12 @@ export class LLMsController {
         entity: TargetEntity.KeyResult,
         input,
         prompt,
+        promptVersion: summarizeKeyResult.PROMPT_VERSION,
         author,
+        estimates: {
+          promptTokens,
+          completionTokens,
+        },
       },
       3,
     )
