b3a04ca36a6e52143b4cce2a8345fac3d92983e1
Rodrigo Antas <rodr.antas@gmail.com>
2023-08-25 12:12:49 -0300

    feat(user-preferences): adding rule to create setting

--- src/core/ports/commands/update-user-main-team-in-preferences.command.ts
+++ src/core/ports/commands/update-user-main-team-in-preferences.command.ts
@@ -1,3 +1,4 @@
+import { Key } from '@core/modules/user/setting/user-setting.enums'
 import { UserSetting } from '@core/modules/user/setting/user-settings.orm-entity'
 
 import { Command } from './base.command'
@@ -5,11 +6,28 @@ import { Command } from './base.command'
 export class UpdateUserMainTeamInPreferencesCommand extends Command<UserSetting> {
   public async execute(userId: string, mainTeamId: string): Promise<UserSetting> {
     const currentSetting = await this.core.user.setting.getOne({ userId })
-    const setting = await this.core.user.setting.update(
-      { id: currentSetting.id },
-      { preferences: { ...currentSetting.preferences, main_team: mainTeamId } },
-    )
+    const setting = currentSetting
+      ? await this.core.user.setting.update(
+          { id: currentSetting.id },
+          { preferences: { ...currentSetting.preferences, main_team: mainTeamId } },
+        )
+      : await this.core.user.setting.createOne({
+          key: Key.LOCALE,
+          value: 'pt-BR',
+          userId,
+          preferences: { main_team: mainTeamId },
+        })
+
+    await this.propagateNewSetting(setting)
 
     return setting
+  }
+
+  private async propagateNewSetting(setting: UserSetting): Promise<void> {
+    if (setting.key === Key.LOCALE) await this.propagateLocale(setting)
+  }
+
+  private async propagateLocale(setting: UserSetting): Promise<void> {
+    return this.core.user.updateUserProperty(setting.userId, setting.key, setting.value)
   }
 }
