068a2980caa4f6589596f405ebd6a28e8c52ab63
diego magalhaes <digengup@gmail.com>
2023-08-24 23:00:41 -0300

    feat(mission-control) fix module

--- src/mission-control/mission-control.module.ts
+++ src/mission-control/mission-control.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 
 import { AWSModule } from '@infrastructure/aws/aws.module'
-import { MissionControlDatabaseModule } from 'src/llm/infra/database/database.module'
-import { HttpModule } from 'src/llm/infra/http/http.module'
+
+import { MissionControlDatabaseModule } from './infra/database/database.module'
 
 @Module({
-  imports: [HttpModule, MissionControlDatabaseModule, AWSModule],
+  imports: [MissionControlDatabaseModule, AWSModule],
 })
 export class MissionControlModule {}
