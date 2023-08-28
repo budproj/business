70c02afa2b6c540f9e54421fe940c24d4cd9c421
diego magalhaes <digengup@gmail.com>
2023-08-28 13:09:02 -0300

    fix(filter-late-checkin) build .

--- src/core/modules/key-result/key-result.provider.ts
+++ src/core/modules/key-result/key-result.provider.ts
@@ -74,7 +74,7 @@ export class KeyResultProvider extends CoreEntityProvider<KeyResult, KeyResultIn
     options?: GetOptions<KeyResult>,
     active = true,
     confidence?: ConfidenceTag,
-    queryRelations?: string[],
+    queryRelations: string[] = [],
   ): Promise<KeyResult[]> {
     const queryOptions = this.repository.marshalGetOptions(options)
     const whereSelector = {
