6e7af82c27430888c350e6ccc2e774c5c61025ca
diego magalhaes <digengup@gmail.com>
2023-09-25 18:36:28 -0300

    update cron job

--- src/mission-control/jobs/assign-tasks.job.ts
+++ src/mission-control/jobs/assign-tasks.job.ts
@@ -8,7 +8,7 @@ export class AssignTasksJobScheduler {
   constructor(private readonly plannerService: TaskPlannerService) {}
 
   // Every Sunday at 00:00
-  @Cron('00 21 * * 1')
+  @Cron('0 0 * * 0')
   async assignTasks() {
     await this.plannerService.execute()
   }
