efcbb6e29968f0cc8d54fd4a60a81fc374c8181d
Danilo Valente <danilovalente96@gmail.com>
2023-09-26 23:56:45 +0200

    config(postgres): configure pool size and idle timeout

--- src/mission-control/infra/database/postgresjs/postgresjs.service.ts
+++ src/mission-control/infra/database/postgresjs/postgresjs.service.ts
@@ -12,6 +12,8 @@ export class PostgresJsService implements OnModuleInit {
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
+      max: 5,
+      idle_timeout: process.env.TYPEORM_CONNECTION_POOL_TIMEOUT_S,
     })
   }
 
