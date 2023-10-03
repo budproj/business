21f7bc5b8d8e30174d620be700857bc6f313488f
Rodrigo Antas <69023532+rodrigoantas@users.noreply.github.com>
2023-10-03 19:31:37 -0300

    feat(qunatities): adding parameter to quantities command be in team resolver (#308)

--- src/core/ports/commands/get-objectives-and-key-results-quantities.command.ts
+++ src/core/ports/commands/get-objectives-and-key-results-quantities.command.ts
@@ -1,14 +1,20 @@
+import { TeamInterface } from '@core/modules/team/interfaces/team.interface'
 import { UserInterface } from '@core/modules/user/user.interface'
 
 import { Command } from './base.command'
 
 export class GetObjectivesAndKeyResultQuantities extends Command<any> {
-  public async execute(user: UserInterface): Promise<any> {
-    const userCompanies = await this.core.team.getUserCompanies(user)
-    const userCompanyIDs = userCompanies.map((company) => company.id)
-    const userReachableTeams = await this.core.team.getDescendantsByIds(userCompanyIDs)
+  public async execute(user: UserInterface, teamId?: TeamInterface['id']): Promise<any> {
+    let userReachableTeamsIds: Array<TeamInterface['id']> = []
 
-    const userReachableTeamsIds = userReachableTeams.map((team) => team.id)
+    if (teamId) {
+      userReachableTeamsIds = [teamId]
+    } else {
+      const userCompanies = await this.core.team.getUserCompanies(user)
+      const userCompanyIDs = userCompanies.map((company) => company.id)
+      const userReachableTeams = await this.core.team.getDescendantsByIds(userCompanyIDs)
+      userReachableTeamsIds = userReachableTeams.map((team) => team.id)
+    }
 
     const keyResultsQuantityPromise =
       this.core.keyResult.getActiveKeyResultsQuantity(userReachableTeamsIds)
