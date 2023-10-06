b44f8d1ad75952d08a37f82b6aa4afc8d5ab584a
diego magalhaes <digengup@gmail.com>
2023-10-06 17:46:23 -0300

    fix(mission-control-check-in-task) add a new constraint: create task if key result is old

--- src/mission-control/infra/database/postgresjs/repositories/core-domain.repositorie.ts
+++ src/mission-control/infra/database/postgresjs/repositories/core-domain.repositorie.ts
@@ -22,26 +22,27 @@ export class PostgresJsCoreDomainRepository implements CoreDomainRepository {
   constructor(private readonly postgres: PostgresJsService) {}
 
   async findOneKeyResultWithOutdatedCheckin({ ownerId, teamId }: findOutdatedKeyResultInput) {
-    const [queryOutput] = await this.postgres.getSqlInstance()<keyResultFromMCContextOutputTable[]>`
+    const sql = this.postgres.getSqlInstance()
+
+    const [queryOutput] = await sql<keyResultFromMCContextOutputTable[]>`
       WITH latest_checkin AS (
-        SELECT DISTINCT ON (krci.key_result_id, krci.created_at) krci.*, kr.created_at AS key_result_created_at
-        FROM key_result kr
-        INNER JOIN objective o ON o.id = kr.objective_id
-        INNER JOIN cycle c ON c.id = o.cycle_id
-        INNER JOIN key_result_check_in krci ON kr.id = krci.key_result_id
-        WHERE kr.team_id = ${teamId}
-          AND kr.owner_id = ${ownerId}
-          AND kr.mode = ${KeyResultMode.PUBLISHED}
-          AND o.mode = ${ObjectiveMode.PUBLISHED}
-          AND c.active = true
-        ORDER BY krci.key_result_id, krci.created_at DESC
-      )
-      SELECT ck.key_result_id AS id
-      FROM latest_checkin ck
-      WHERE ck.created_at < NOW() - INTERVAL '7 days'
-        AND ck.key_result_created_at < NOW() - INTERVAL '7 days'
-      LIMIT 1;
-  `
+          SELECT krci.created_at, kr.id, kr.created_at AS key_result_created_at
+          FROM key_result kr
+          INNER JOIN objective o ON o.id = kr.objective_id
+          INNER JOIN cycle c ON c.id = o.cycle_id
+          LEFT JOIN key_result_check_in krci ON kr.id = krci.key_result_id
+          WHERE kr.team_id = ${teamId}
+                AND kr.mode = ${KeyResultMode.PUBLISHED}
+                AND kr.owner_id = ${ownerId}
+                AND c.active = true
+                AND o.mode = ${ObjectiveMode.PUBLISHED}
+                ORDER BY krci.key_result_id, krci.created_at DESC 
+                LIMIT 1)
+        SELECT ck.id
+        FROM latest_checkin ck
+        WHERE ck.created_at < NOW() - INTERVAL '7 days'
+        OR (ck.created_at IS NULL AND ck.key_result_created_at < NOW() - INTERVAL '7 days');`
+
     return queryOutput
   }
 
