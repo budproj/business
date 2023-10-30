5b8144a1059358a1047b76d950235c6c77b8bf29
diego magalhaes <digengup@gmail.com>
2023-10-30 19:21:42 -0300

    chore: remove cache from some user methods

--- src/mission-control/infra/http/context/interceptors/add-http-context-to-user.interceptor.ts
+++ src/mission-control/infra/http/context/interceptors/add-http-context-to-user.interceptor.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs'
 import { PolicyAdapter } from '@adapters/policy/policy.adapter'
 import { UserWithContext } from '@adapters/state/interfaces/user.interface'
 import { CoreProvider } from '@core/core.provider'
-import { Cacheable } from '@lib/cache/cacheable.decorator'
 
 import { HTTPRequest } from '../types'
 
@@ -32,7 +31,7 @@ export class AddHTTPContextToUserInterceptor implements NestInterceptor {
   }
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
-  @Cacheable(({ user }) => [user.id, user.token.sub, user.token.permissions], 15 * 60)
+  // @Cacheable(({ user }) => [user.id, user.token.sub, user.token.permissions], 15 * 60)
   private async getRequestUser(request: HTTPRequest): Promise<UserWithContext> {
     const { teams, ...user } = await this.core.user.getUserFromSubjectWithTeamRelation(
       request.user.token.sub,
