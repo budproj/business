ec2c8ff5053c6c55b34a7be4967ab428269329c3
Igor Omote <iomote@cuidas.com.br>
2024-01-29 13:56:23 -0300

    perf(commands): write some of our front logic into sql queries

--- src/core/ports/commands/get-cycle-status.command.ts
+++ src/core/ports/commands/get-cycle-status.command.ts
@@ -56,8 +56,65 @@ export class GetCycleStatusCommand extends BaseStatusCommand {
       [['DESC']],
     )
 
+    /* with latest_check_in as (
+      select distinct on (krci.key_result_id) * from public.key_result_check_in krci
+      where krci.created_at < '2024-01-24'
+      order by krci.key_result_id, krci.created_at desc
+    ),
+    key_result_status as (
+      select 
+        kr.id, 
+        case when lci.created_at < current_date - interval '6' day then true else false end as is_outdated,  
+        c.active,
+        -- lci.*,
+        lci.created_at as last_check_in_date,
+        case 
+          when lci.value = kr.goal then 100 
+          when kr.goal = kr.initial_value then 0
+        else
+        100 * (coalesce(lci.value,0) - kr.initial_value) / (kr.goal - kr.initial_value) end as progress,
+        lci.confidence,
+        kr.objective_id,
+        kr.team_id
+      from public.key_result kr 
+      left join latest_check_in lci on kr.id = lci.key_result_id
+      join public.objective o on kr.objective_id  = o.id
+      join public.cycle c on o.cycle_id = c.id
+      --where lci.confidence <> '-100'
+    ),
+    objective_status as (
+      select 
+        krs.objective_id,
+        krs.team_id,
+        cy.id as cycle_id,
+        cy.team_id as company_id,
+        bool_or(is_outdated) as is_outdated,
+        max(last_check_in_date) as last_check_in_date,
+        avg(progress) as progress,
+        min(confidence) as confidence
+      from key_result_status krs 
+      join objective o on krs.objective_id = o.id
+      join cycle cy on o.cycle_id = cy.id
+      where o.mode = 'PUBLISHED' and cy.active is true
+      group by krs.objective_id, krs.team_id, cy.id, cy.team_id
+    ),
+    cycle_status as (
+      select 
+        o.cycle_id,
+        o.company_id,
+        bool_or(is_outdated) as is_outdated,
+        max(last_check_in_date) as last_check_in_date,
+        greatest(avg(progress), 0) as progress,
+        min(confidence) as confidence
+      from objective_status o
+      where o.team_id is not null
+      group by o.cycle_id, o.company_id
+    )
+    select * from cycle_status cs 
+      where cs.company_id = 'f6790108-2b16-4077-bed0-103fc38175dd' */
+      
     const keyResults = await this.core.keyResult.getWithRelationFilters(filters, orderAttributes)
 
     return this.removeKeyResultCheckInsBeforeDate(keyResults, options.date)
-  }
+  } 
 }
