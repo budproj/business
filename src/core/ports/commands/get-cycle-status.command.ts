1ad41c98357653ec242d809e5ce7adcbf84683b2
Igor Omote <iomote@cuidas.com.br>
2024-02-06 15:00:05 -0300

    remove unnecessary comments

--- src/core/ports/commands/get-cycle-status.command.ts
+++ src/core/ports/commands/get-cycle-status.command.ts
@@ -1,6 +1,5 @@
 import { GetStatusOptions, Status } from '@core/interfaces/status.interface'
-import { KeyResultMode } from '@core/modules/key-result/enums/key-result-mode.enum'
-import { KeyResult } from '@core/modules/key-result/key-result.orm-entity'
+import { KeyResultCheckIn } from '@core/modules/key-result/check-in/key-result-check-in.orm-entity'
 import { BaseStatusCommand } from '@core/ports/commands/base-status.command'
 import { Cacheable } from '@lib/cache/cacheable.decorator'
 import { Stopwatch } from '@lib/logger/pino.decorator'
@@ -15,7 +14,6 @@ export class GetCycleStatusCommand extends BaseStatusCommand {
     cycleID: string,
     options: GetStatusOptions = this.defaultOptions,
   ): Promise<Status> {
-    console.log('1. blah')
     const row = await this.core.entityManager.query(
       `
       WITH latest_check_in AS
@@ -23,11 +21,13 @@ export class GetCycleStatusCommand extends BaseStatusCommand {
         FROM public.key_result_check_in krci
         ORDER BY krci.key_result_id,
                   krci.created_at DESC),
-          latest_check_in_by_cycle AS
-        (SELECT DISTINCT ON (o.cycle_id) *
+      latest_check_in_by_cycle AS
+        (SELECT DISTINCT ON (o.cycle_id) krci.*, o.cycle_id
         FROM public.key_result_check_in krci
         JOIN public.key_result kr ON krci.key_result_id = kr.id
         JOIN public.objective o ON kr.objective_id = o.id
+        WHERE o.mode = 'PUBLISHED'
+          AND o.team_id IS NOT NULL 
         ORDER BY o.cycle_id,
                   krci.created_at DESC),
           key_result_status AS
@@ -87,131 +87,41 @@ export class GetCycleStatusCommand extends BaseStatusCommand {
         GROUP BY o.cycle_id,
                   o.company_id)
       SELECT cs.*,
-            to_json(lcibc.*) AS latestCheckIn
+            to_json(lcibc.*) AS latest_check_in
       FROM cycle_status cs
       JOIN latest_check_in_by_cycle lcibc ON cs.cycle_id = lcibc.cycle_id
       WHERE cs.cycle_id = $1
       `,
       [cycleID],
     )
-    const keyResults = (await this.getKeyResultsFromCycle(cycleID, options)) as any
 
-    const filteredKeyResults = keyResults.filter((keyResult) => keyResult.teamId !== null)
+    const latestCheckInNew = new KeyResultCheckIn()
+    latestCheckInNew.id = row[0].latest_check_in.id
+    latestCheckInNew.value = row[0].latest_check_in.value
+    latestCheckInNew.confidence = row[0].latest_check_in.confidence
+    latestCheckInNew.comment = row[0].latest_check_in.comment
+    latestCheckInNew.createdAt = row[0].latest_check_in.created_at
+    latestCheckInNew.keyResultId = row[0].latest_check_in.id
+    latestCheckInNew.userId = row[0].latest_check_in.id
+    latestCheckInNew.parentId = row[0].latest_check_in.parent_id
+    latestCheckInNew.previousState = row[0].latest_check_in.previous_state
 
-    const [cycleCheckIns, progresses, confidences] = await this.unzipKeyResultGroup(
-      filteredKeyResults,
-    )
-
-    const latestCheckIn = this.getLatestCheckInFromList(cycleCheckIns)
-    const isOutdated = this.isOutdated(latestCheckIn)
-    const isActive = keyResults[0]?.objective?.cycle?.active ?? this.defaultStatus.isActive
-    console.log('2. blah')
-    console.log(row[0])
-    console.log({ // tá enroscado aqui
+    console.log('1. blah blah2')
+    console.log({
       isOutdated: row[0].is_outdated,
       isActive: row[0].is_active,
-      latestCheckIn: row[0].latestCheckIn,
-      reportDate: row[0].latestCheckIn.created_at,
+      latestCheckIn: latestCheckInNew,
+      reportDate: row[0].latest_check_in.created_at,
       progress: row[0].progress,
       confidence: row[0].confidence,
     })
-    console.log('3. O que eu fiz acima, o gabarito abaixo')
-    console.log({
-      isOutdated,
-      isActive,
-      latestCheckIn,
-      reportDate: latestCheckIn?.createdAt,
-      progress: this.getAverage(progresses),
-      confidence: this.getMin(confidences),
-    })
     return {
-      isOutdated,
-      isActive,
-      latestCheckIn,
-      reportDate: latestCheckIn?.createdAt,
-      progress: this.getAverage(progresses),
-      confidence: this.getMin(confidences),
+      isOutdated: row[0].is_outdated,
+      isActive: row[0].is_active,
+      latestCheckIn: latestCheckInNew,
+      reportDate: row[0].latest_check_in.created_at,
+      progress: row[0].progress,
+      confidence: row[0].confidence,
     }
-  }
-
-  private async getKeyResultsFromCycle(
-    cycleID: string,
-    options: GetStatusOptions,
-  ): Promise<KeyResult[]> {
-    const filters = {
-      keyResult: {
-        createdAt: options.date,
-        mode: KeyResultMode.PUBLISHED,
-      },
-      cycle: {
-        id: cycleID,
-      },
-    }
-    const orderAttributes = this.zipEntityOrderAttributes(
-      ['keyResultCheckIn'],
-      [['createdAt']],
-      [['DESC']],
-    )
-
-    /* With latest_check_in as (
-      select distinct on (krci.key_result_id) * from public.key_result_check_in krci
-      where krci.created_at < '2024-01-24'
-      order by krci.key_result_id, krci.created_at desc
-    ),
-    key_result_status as (
-      select 
-        kr.id, 
-        case when lci.created_at < current_date - interval '6' day then true else false end as is_outdated,  
-        c.active,
-        -- lci.*,
-        lci.created_at as last_check_in_date,
-        case 
-          when lci.value = kr.goal then 100 
-          when kr.goal = kr.initial_value then 0
-        else
-        100 * (coalesce(lci.value,0) - kr.initial_value) / (kr.goal - kr.initial_value) end as progress,
-        lci.confidence,
-        kr.objective_id,
-        kr.team_id
-      from public.key_result kr 
-      left join latest_check_in lci on kr.id = lci.key_result_id
-      join public.objective o on kr.objective_id  = o.id
-      join public.cycle c on o.cycle_id = c.id
-      --where lci.confidence <> '-100'
-    ),
-    objective_status as (
-      select 
-        krs.objective_id,
-        krs.team_id,
-        cy.id as cycle_id,
-        cy.team_id as company_id,
-        bool_or(is_outdated) as is_outdated,
-        max(last_check_in_date) as last_check_in_date,
-        avg(progress) as progress,
-        min(confidence) as confidence
-      from key_result_status krs 
-      join objective o on krs.objective_id = o.id
-      join cycle cy on o.cycle_id = cy.id
-      where o.mode = 'PUBLISHED' and cy.active is true
-      group by krs.objective_id, krs.team_id, cy.id, cy.team_id
-    ),
-    cycle_status as (
-      select 
-        o.cycle_id,
-        o.company_id,
-        bool_or(is_outdated) as is_outdated,
-        max(last_check_in_date) as last_check_in_date,
-        greatest(avg(progress), 0) as progress,
-        min(confidence) as confidence
-      from objective_status o
-      where o.team_id is not null
-      group by o.cycle_id, o.company_id
-    )
-    select * from cycle_status cs 
-      where cs.company_id = 'f6790108-2b16-4077-bed0-103fc38175dd' */
-
-    const keyResults = await this.core.keyResult.getWithRelationFilters(filters, orderAttributes)
-
-    return this.removeKeyResultCheckInsBeforeDate(keyResults, options.date)
   }
 }
