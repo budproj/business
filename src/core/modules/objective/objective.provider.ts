c44fba5422c2d361645d3a75a7944e940aefcf0e
Kevin <kevin.c.souza@gmail.com>
2025-06-12 14:18:01 -0300

    debug objective

--- src/core/modules/objective/objective.provider.ts
+++ src/core/modules/objective/objective.provider.ts
@@ -147,6 +147,7 @@ export class ObjectiveProvider extends CoreEntityProvider<Objective, ObjectiveIn
   }
 
   public async createObjective(objectiveData: ObjectiveInterface): Promise<Objective> {
+    console.log(objectiveData)
     const [queryResult] = await this.create(objectiveData)
     return queryResult
   }
